79df4b668bdc06f1fe12cc015cccf962cb3ce108
cooker0910 <cooker98910@gmail.com>
2022-08-04 22:28:58 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { FaTwitterSquare, FaDiscord } from 'react-icons/fa';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 import PreviousImage from './Components/PreviousImage'
-import CUDDLE_CLUB from './assets/cuddle_club.gif';
+import MONSTER from './assets/monster-3.png'
 import { ReactComponent as Opensea } from './assets/opensea.svg'
 
 let web3Modal;
@@ -22,7 +22,7 @@ const App = () => {
       <div className={styles.container}>
         <div className={styles.bear}>
           <PreviousImage
-            src={CUDDLE_CLUB}
+            src={MONSTER}
             alt=""
             placeholder="GIF"
             errorImage=""
@@ -30,8 +30,8 @@ const App = () => {
           />
         </div>
         <div className={`d-flex flex-column ${styles.rightpan}`}>
-          <h1 className={styles.title}>Cuddle Club</h1>
-          <div className={styles.text}>6969 Cuddle Bears dominating in the bear market. First 4000 free, then 2969 for 0.005 each. 3 mints per wallet</div>
+          <h1 className={styles.title}>Lab Monster</h1>
+          {/* <div className={styles.text}>6969 Cuddle Bears dominating in the bear market. First 4000 free, then 2969 for 0.005 each. 3 mints per wallet</div> */}
           <div className="d-flex justify-content-center">
             <div className={styles.mainButton}>
                 <button>CONNECT WALLET</button>
@@ -40,7 +40,7 @@ const App = () => {
         </div>
       </div>
       <div className={styles.footer}>
-        <a href='https://twitter.com/CuddleClubNFT' target='_blank'>
+        <a href='#' target='_blank'>
           <FaTwitterSquare className={styles.socialTwitter} />
         </a>
         <a href='#'>
